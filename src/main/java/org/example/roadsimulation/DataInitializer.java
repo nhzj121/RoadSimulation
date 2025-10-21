4ebcef0317ecfce529bbd94ae88dc4bafcbff1e5
nhzj121 <195225527@qq.com>
2025-10-21 08:48:06 +0800

    本地更改

--- src/main/java/org/example/roadsimulation/DataInitializer.java
+++ src/main/java/org/example/roadsimulation/DataInitializer.java
@@ -1,7 +1,4 @@
 package org.example.roadsimulation;
 
-import org.example.roadsimulation.entity.Action;
-import org.example.roadsimulation.repository.ActionRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+
 
--- src/main/java/org/example/roadsimulation/RoadSimulationApplication.java
+++ src/main/java/org/example/roadsimulation/RoadSimulationApplication.java
@@ -8,6 +8,10 @@ public class RoadSimulationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoadSimulationApplication.class, args);
+        /*状态同步机制缺失
+        问题：各实体状态更新缺乏事务性保证：当前状态更新分散在不同地方,如果某一步失败，状态将不一致
+        需要尝试在Assignment中统一管理状态 ToDo
+        */
 	}
 
 }
