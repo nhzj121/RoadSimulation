ad48a35646c905b1b0107cc6a11efe7e6e8d1762
nhzj121 <195225527@qq.com>
2026-04-11 17:19:08 +0800

    Revert "Marsh p 4 3"

--- src/main/java/org/example/roadsimulation/dto/ShipmentItemDTO.java
+++ src/main/java/org/example/roadsimulation/dto/ShipmentItemDTO.java
@@ -1,95 +1,60 @@
 package org.example.roadsimulation.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Data
 public class ShipmentItemDTO {
+    @Setter
+    @Getter
     private Long id;
+    @Setter @Getter
     private String name;
+    @Setter @Getter
     private String sku;
+    @Setter @Getter
     private Integer qty;
+    @Setter @Getter
     private Double weight;
+    @Setter @Getter
     private Double volume;
 
     // 关联信息
+    @Setter @Getter
     private Long shipmentId;
+    @Setter @Getter
     private String shipmentRefNo;
+    @Setter @Getter
     private Long goodsId;
+    @Setter @Getter
     private String goodsName;
+    @Setter @Getter
     private Long assignmentId;
 
     // 位置信息
+    @Setter @Getter
     private Long originPOIId;
+    @Setter @Getter
     private String originPOIName;
+    @Setter @Getter
     private Long destPOIId;
+    @Setter @Getter
     private String destPOIName;
 
     // 状态信息
-    private String status;
+    @Setter @Getter
+    private String status; // LOADED, IN_TRANSIT, UNLOADED, etc.
+    @Setter @Getter
     private LocalDateTime loadedTime;
+    @Setter @Getter
     private LocalDateTime unloadedTime;
 
     // 元数据
+    @Setter @Getter
     private LocalDateTime createdTime;
+    @Setter @Getter
     private LocalDateTime updatedTime;
-
-    // Getters & Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getSku() { return sku; }
-    public void setSku(String sku) { this.sku = sku; }
-
-    public Integer getQty() { return qty; }
-    public void setQty(Integer qty) { this.qty = qty; }
-
-    public Double getWeight() { return weight; }
-    public void setWeight(Double weight) { this.weight = weight; }
-
-    public Double getVolume() { return volume; }
-    public void setVolume(Double volume) { this.volume = volume; }
-
-    public Long getShipmentId() { return shipmentId; }
-    public void setShipmentId(Long shipmentId) { this.shipmentId = shipmentId; }
-
-    public String getShipmentRefNo() { return shipmentRefNo; }
-    public void setShipmentRefNo(String shipmentRefNo) { this.shipmentRefNo = shipmentRefNo; }
-
-    public Long getGoodsId() { return goodsId; }
-    public void setGoodsId(Long goodsId) { this.goodsId = goodsId; }
-
-    public String getGoodsName() { return goodsName; }
-    public void setGoodsName(String goodsName) { this.goodsName = goodsName; }
-
-    public Long getAssignmentId() { return assignmentId; }
-    public void setAssignmentId(Long assignmentId) { this.assignmentId = assignmentId; }
-
-    public Long getOriginPOIId() { return originPOIId; }
-    public void setOriginPOIId(Long originPOIId) { this.originPOIId = originPOIId; }
-
-    public String getOriginPOIName() { return originPOIName; }
-    public void setOriginPOIName(String originPOIName) { this.originPOIName = originPOIName; }
-
-    public Long getDestPOIId() { return destPOIId; }
-    public void setDestPOIId(Long destPOIId) { this.destPOIId = destPOIId; }
-
-    public String getDestPOIName() { return destPOIName; }
-    public void setDestPOIName(String destPOIName) { this.destPOIName = destPOIName; }
-
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-
-    public LocalDateTime getLoadedTime() { return loadedTime; }
-    public void setLoadedTime(LocalDateTime loadedTime) { this.loadedTime = loadedTime; }
-
-    public LocalDateTime getUnloadedTime() { return unloadedTime; }
-    public void setUnloadedTime(LocalDateTime unloadedTime) { this.unloadedTime = unloadedTime; }
-
-    public LocalDateTime getCreatedTime() { return createdTime; }
-    public void setCreatedTime(LocalDateTime createdTime) { this.createdTime = createdTime; }
-
-    public LocalDateTime getUpdatedTime() { return updatedTime; }
-    public void setUpdatedTime(LocalDateTime updatedTime) { this.updatedTime = updatedTime; }
 }
